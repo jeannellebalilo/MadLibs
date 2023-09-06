57c46cc353cf6ed1a1dd32c71a65dd883771bf7d
jeannellebalilo <jeannellebalilo@gmail.com>
2023-09-06 13:02:20 -0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        PriorityQueue<String> nouns = new PriorityQueue<String>();
+        PriorityQueue<String> adjectives = new PriorityQueue<String>();
+        PriorityQueue<String> verbs = new PriorityQueue<String>();
+
+        System.out.println("Type an adjective: ");
+        
+    }
+}
