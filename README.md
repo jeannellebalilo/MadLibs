18e1351943b487ca7de3bff196e2bf67f75e682b
jeannellebalilo <jeannellebalilo@gmail.com>
2023-09-06 14:05:20 -0700

    getWord function

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
-## Getting Started
+## Mad Libs Java App
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+In this project, I made a Mad Libs word game which takes user inputs and uses them to make a short story. The result is usually funny, sometimes strange.
 
-## Folder Structure
+## "Roses are Red" poem example
 
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Roses are [adjective], [noun] are blue. [noun] is [adjective], and so are you!
--- src/App.java
+++ src/App.java
@@ -1,13 +1,27 @@
-import java.util;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        PriorityQueue<String> nouns = new PriorityQueue<String>();
-        PriorityQueue<String> adjectives = new PriorityQueue<String>();
-        PriorityQueue<String> verbs = new PriorityQueue<String>();
+        Queue<String> words = new LinkedList<>();
 
-        System.out.println("Type an adjective: ");
-        
+        Scanner scan = new Scanner(System.in);
+        words.add(getWord(scan, "adjective"));
+        words.add(getWord(scan, "noun"));
+        words.add(getWord(scan, "noun"));
+        words.add(getWord(scan, "adjective"));
+
+        System.out.println("Roses are " + words.remove() + ", " + 
+                            words.remove() + "s are blue. " + 
+                            words.remove() + " is " + 
+                            words.remove() + ", and so are you!");
+    }
+
+    // getWord() uses a Scanner object to retrieve a word from a user.
+    // input requires a Scanner, and a "type" (adjective, noun, verb, etc.)
+    static String getWord(Scanner s, String type) {
+        System.out.print("Type an " + type + ": ");
+        String word = s.nextLine();
+        return word;
     }
 }
